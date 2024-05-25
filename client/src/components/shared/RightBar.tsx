daed03bfd0ebfb21883dc0bb9ce334bbe23a3a3d
partharora1610 <partharora16102001@gmail.com>
2024-05-25 18:56:17 +0530

    frrrr.....

--- client/src/components/shared/RightBar.tsx
+++ client/src/components/shared/RightBar.tsx
@@ -14,27 +14,37 @@ const RightBar = () => {
   )
 }
 
-const VENDORS = [
+const GUESTS = [
   "Mr Mukesh Bansal",
   "Mr Nitin kamath",
   "Wedding Venue Service",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
+  "Crazy Events",
   "Crazy Events",
 ]
 
 const VendorList = () => {
   return (
     <div className="bg-white py-4 px-2 rounded-sm">
-      <h3 className="font-medium text-xl mb-4">Event Vendors</h3>
+      <h3 className="font-medium text-xl mb-4 pl-3">Event Guest</h3>
       <div className="flex flex-col gap-4">
-        {VENDORS.map((vendor) => {
+        {GUESTS.map((vendor) => {
           return (
             <div className="flex gap-2 items-center px-2 py-3 rounded-md">
               <div className="w-[48px] h-[48px] bg-gray-800 rounded-full"></div>
               <div>
                 <p className="text-base">{vendor}</p>
-                <p className="text-gray-500">
-                  Responsible for <span className="underline">caterting</span>
-                </p>
               </div>
             </div>
           )
