5d849af21e21dcdb5152453577a1f10dda41878c
partharora1610 <partharora16102001@gmail.com>
2024-05-29 16:34:26 +0530

    added safelist in tailwind config

--- client/src/app/(root)/(auth)/event/(event)/[eventId]/themes/page.tsx
+++ client/src/app/(root)/(auth)/event/(event)/[eventId]/themes/page.tsx
@@ -45,92 +45,20 @@ const PrimaryColorSelection = () => {
             </p>
           </div>
           <div className="flex gap-4">
+            <div className={`w-12 h-12 rounded-md bg-[${selectedColor}]`}></div>
             <div
-              className={`w-12 h-12 rounded-md bg-[${selectedColor.color}]`}
-            ></div>
-            <div
-              className={`w-12 h-12 rounded-md bg-[${selectedColor.color}]/10`}
+              className={`w-12 h-12 rounded-md bg-[${selectedColor}]/10`}
             ></div>
           </div>
         </div>
       </div>
-
-      {/* bg-[#dc0e63] */}
-      {/* bg-[#FF6B00] */}
-      {/* bg-[#ae3ec9] */}
-      {/* bg-[#f03e3e] */}
-      {/* bg-[#1971c2] */}
-      {/* bg-[#099268] */}
-      {/* bg-[#0c8599] */}
-      {/* hover:bg-[#dc0e63] */}
-      {/* hover:bg-[#FF6B00] */}
-      {/* hover:bg-[#ae3ec9] */}
-      {/* hover:bg-[#f03e3e] */}
-      {/* hover:bg-[#1971c2] */}
-      {/* hover:bg-[#099268] */}
-      {/* hover:bg-[#0c8599] */}
-
-      {/* hover:bg-[#dc0e63]/10 */}
-      {/* hover:bg-[#FF6B00]/10 */}
-      {/* hover:bg-[#ae3ec9]/10 */}
-      {/* hover:bg-[#f03e3e]/10 */}
-      {/* hover:bg-[#1971c2]/10 */}
-      {/* hover:bg-[#099268]/10 */}
-      {/* hover:bg-[#0c8599]/10 */}
-
-      {/* bg-[#ae3ec9]/10 */}
-      {/* bg-[#dc0e63]/10 */}
-      {/* bg-[#FF6B00]/10 */}
-      {/* bg-[#f03e3e]/10 */}
-      {/* bg-[#1971c2]/10 */}
-      {/* bg-[#099268]/10 */}
-      {/* bg-[#0c8599]/10 */}
-
-      {/* bg-[#ae3ec9]/10 */}
-      {/* bg-[#dc0e63]/10 */}
-      {/* bg-[#FF6B00]/10 */}
-      {/* bg-[#f03e3e]/10 */}
-      {/* bg-[#1971c2]/10 */}
-      {/* bg-[#099268]/10 */}
-      {/* bg-[#0c8599]/10 */}
-
-      {/* bg-[#ae3ec9]/10 */}
-      {/* bg-[#dc0e63]/10
-
-      {/* text-[#dc0e63] */}
-      {/* text-[#FF6B00] */}
-      {/* text-[#ae3ec9] */}
-      {/* text-[#f03e3e] */}
-      {/* text-[#1971c2] */}
-      {/* text-[#099268] */}
-      {/* text-[#0c8599] */}
-
-      {/* bg-[#ae3ec9]/10 */}
-      {/* bg-[#dc0e63]/10 */}
-      {/* bg-[#FF6B00]/10 */}
-      {/* bg-[#f03e3e]/10 */}
-      {/* bg-[#1971c2]/10 */}
-      {/* bg-[#099268]/10 */}
-      {/* bg-[#0c8599]/10 */}
-
-      {/* border-[#dc0e63] */}
-      {/* bg-[#ae3ec9]/10 */}
-      {/* bg-[#dc0e63]/10 */}
-      {/* bg-[#FF6B00]/10 */}
-      {/* bg-[#f03e3e]/10 */}
-      {/* bg-[#1971c2]/10 */}
-      {/* bg-[#099268]/10 */}
-      {/* bg-[#0c8599]/10 */}
-
-      <p>{JSON.stringify(primaryColor)}</p>
-      <p>{JSON.stringify(selectedColor)}</p>
 
       <div className="mt-4 flex flex-wrap gap-20">
         {PRIMARYCOLOR.map((color) => (
           <div
             onClick={() => setSelectedColor(color.color)}
             className={`flex gap-2 items-center border-2 border-transparent cursor-pointer p-2 rounded-md ${
-              color.color == selectedColor ? " border-gray-500" : ""
+              color.color == selectedColor ? "border-gray-600" : ""
             } `}
           >
             <div className={`w-14 h-14 rounded-md bg-[${color.color}]`}></div>
@@ -139,7 +67,12 @@ const PrimaryColorSelection = () => {
       </div>
 
       <div className="flex justify-end mt-16">
-        <Button onClick={changePrimaryColorHandler}>Save Theme</Button>
+        <Button
+          disabled={primaryColor == selectedColor}
+          onClick={changePrimaryColorHandler}
+        >
+          Save Theme
+        </Button>
       </div>
     </div>
   )
